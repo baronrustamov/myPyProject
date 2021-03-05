e4422a7d2e98ca92127341bcdadaa8117f8bb77c
baronrustamov <baronrustamov@gmail.com>
2021-03-05 13:56:00 +0300

    tbot upd 4

--- tbot.py
+++ tbot.py
@@ -6,21 +6,17 @@ from telebot import types
 
 bot = telebot.TeleBot('251180362:AAG2Y1VTSazHOoEFhFmJVpnhvnc5GiqEQq0')
 
-#keyboard1 = telebot.types.ReplyKeyboardMarkup()
-#keyboard1.row('Привет', 'Пока', 'я тебя люблю')
 
-# Заготовки для трёх предложений
-first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
-second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
-second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
-third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."] # Метод, который получает сообщения и обрабатывает их @
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Привет', 'Пока', 'я тебя люблю')
 
+'''
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     # Если написали «Привет»
     if message.text == "Привет":
         #Пишем приветствие
-        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+        bot.send_message(message.from_user.id, "Привет.")
         # Готовим кнопки
         keyboard = types.InlineKeyboardMarkup()
         # По очереди готовим текст и обработчик для каждого знака зодиака
@@ -54,8 +50,6 @@ def get_text_messages(message):
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши Привет")
     else: bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-    # Обработчик нажатий на кнопки
-
 # Обработчик нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -65,13 +59,13 @@ def callback_worker(call):
         msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third)
         # Отправляем текст в Телеграм
         bot.send_message(call.message.chat.id, msg)
+'''
 
 print("Starting bot")
 
-'''
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
     print('start message ok')
 
 @bot.message_handler(content_types=['text'])
@@ -82,11 +76,10 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Прощай, создатель')
     elif message.text.lower() == 'я тебя люблю':
         bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
-'''
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
     print(message)
 
-#bot.polling(none_stop=True, interval=0)
-bot.polling()
+bot.polling(none_stop=True, interval=0)
+#bot.polling()
